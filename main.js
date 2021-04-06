5de49b70941bd24b3e5f16f47150cd36c6e9f0bc
MrPopiette <maxime.monnier@epsi.fr>
2021-04-06 11:23:09 +0200

    sprint 2

--- main.js
+++ main.js
@@ -1,6 +1,61 @@
 var displayTitle = "Hello, world !"
 
 const helloWorld = x => console.log(x)
+const displayDict = paysTVADict => console.log(paysTVADict)
+
+var paysTVADict = []
+
+paysTVADict.push({
+    pays: "Belgique",
+    tva: 21
+});
+
+paysTVADict.push({
+    pays: "Bulgarie",
+    tva: 20
+});
+
+paysTVADict.push({
+    pays: "Danemark",
+    tva: 25
+});
+
+paysTVADict.push({
+    pays: "Allemagne",
+    tva: 19
+});
+
+paysTVADict.push({
+    pays: "Estonie",
+    tva: 20
+});
+
+paysTVADict.push({
+    pays: "Espagne",
+    tva: 21
+});
+
+paysTVADict.push({
+    pays: "France",
+    tva: 20
+});
+
+paysTVADict.push({
+    pays: "France",
+    tva: 20
+});
+
+paysTVADict.push({
+    pays: "République Tchèque",
+    tva: 21
+});
+
+paysTVADict.push({
+    pays: "Grèce",
+    tva: 23
+});
+
+
 
 helloWorld(displayTitle)
-
+displayDict(paysTVADict)
--- main.test.js
+++ main.test.js
@@ -4,3 +4,14 @@ test("helloWorld", function () {
     var displayTitle = "Hello, world !"
     expect(displayTitle).toEqual("Hello, world !")
 })
+
+test("displayDict", function () {
+    var paysTVADict = []
+
+    paysTVADict.push({
+        pays: "Belgique",
+        tva: 21
+    });
+
+    expect(paysTVADict).toEqual([{ pays: 'Belgique', tva: 21 }])
+})
